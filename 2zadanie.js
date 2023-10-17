46f053e57d3a0630cebf5612103f7f0c08cb7360
yeezus2408 <129648151+yeezus2408@users.noreply.github.com>
2023-10-17 13:47:42 +0500

    Add files via upload

---
+++ 2zadanie.js
@@ -0,0 +1,16 @@
+var num = 2;
+
+switch(num) {
+    case 1:
+        console.log("Пики");
+        break
+    case 2:
+        console.log("Трефы");
+        break
+    case 3:
+        console.log("бубны");
+        break
+    case 4:
+        console.log("Черви");
+        break
+}
---
+++ 3zadanie.js
@@ -0,0 +1,47 @@
+var num = 2;
+var num2 = 6;
+
+switch(num2) {
+    case 6:
+        console.log("Шестерка");
+        break
+    case 7:
+        console.log("Семерка");
+        break
+    case 8:
+        console.log("Восьмерка");
+        break
+    case 9:
+        console.log("девятка");
+        break
+    case 10:
+        console.log("десятка");
+        break
+    case 11:
+        console.log("Валет");
+        break
+    case 12:
+        console.log("Дама");
+        break
+    case 13:
+        console.log("Король");
+        break
+    case 14:
+        console.log("Туз");
+        break
+}
+
+switch(num) {
+    case 1:
+        console.log("Пики");
+        break
+    case 2:
+        console.log("Трефы");
+        break
+    case 3:
+        console.log("бубны");
+        break
+    case 4:
+        console.log("Черви");
+        break
+}
